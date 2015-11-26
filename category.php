6bc99057700a784d7868780ec8d6b6aec0fcb03c
EpicNameBro <nintendo_jon64@hotmail.com>
2015-11-26 15:37:57 -0500

    fixed bootstrap columns

--- category.php
+++ category.php
@@ -56,34 +56,35 @@
 		<div class="wrap">
 			<?php include 'header.php';?>
 			<div class="main">
-				</br>
+				<br/>
+				<br/>
 
 				<ol class="breadcrumb">
 					<li><a href="index.php"><i class="glyphicon glyphicon-home"></i></a></li>
 					<li><a href="category.php?category_id=<?= $_GET['category_id'] ?>"><?= $categoryname ?></a></li>
 				</ol>
 
-				<!-- Filters -->
+				<!-- Filters
 				<div class="col-md-2">
 					<div class="panel panel-default">
 						<div class="panel-body">
 
 						</div>
 					</div>
-				</div>
+				</div> -->
 
 				<!-- Results -->
-				<div class="col-md-10">
+				<div class="col-md-12">
 					<div class="panel panel-default">
 						<div class="panel-body">
-							<div class="row">
+							<!--<div class="row">
 								<div class="col-md-3">
 									<select class="form-control">
 
 									</select>
 								</div>
-							</div>
-							</br>
+							</div>-->
+							<br/>
 							<?php
 								//display all products from the category
 								for($i = 0 ; $i < count($products) ; $i++)
@@ -91,13 +92,32 @@
 					?>				<div class="col-md-12 panel panel-default">
 										<div class="panel-body" style="text-align: center;">
 											<form role="form" method="POST" action="cart.php">
-												<a href="preview.php?product_id=<?= $products[$i]['product_id'] ?>">
-													<img style="height: 125px; width: auto" class="col-md-3 vcenter" src="<?= $products[$i]['Image_Url'] ?>"/>
-													<span class="col-md-3 vcenter"><b><h4><?= $products[$i]['product_name'] ?></h4></b></span>
-												</a>
-												<span class="col-md-3 vcenter"><b>$<?= $products[$i]['Price'] ?></b></span>
-												
-												<button name="addcart" value="<?= $products[$i]['product_id'] ?>" type="submit" class="addcart col-md-3 btn btn-info vcenter"><span class="glyphicon glyphicon-shopping-cart"></span> ADD TO CART</button>
+												<div class="row">
+													
+													<a href="preview.php?product_id=<?= $products[$i]['product_id'] ?>">
+														<div class="col-md-3 vcenter">
+															<img style="height: 125px; width: auto" src="<?= $products[$i]['Image_Url'] ?>"/>
+														</div>
+													</a>
+													<a href="preview.php?product_id=<?= $products[$i]['product_id'] ?>">
+														<div class="col-md-3 vcenter">
+															<span><b><h4><?= $products[$i]['product_name'] ?></h4></b></span>
+														</div>
+													</a>
+													
+
+
+													<div class="col-md-3 vcenter">
+														<span><b>$<?= $products[$i]['Price'] ?></b></span>
+													</div>
+
+
+													<div class="col-md-2 vcenter">
+														<button name="addcart" value="<?= $products[$i]['product_id'] ?>" type="submit" class="addcart btn btn-info"><span class="glyphicon glyphicon-shopping-cart"></span> ADD TO CART</button>
+													</div>
+													
+													
+												</div>
 											</form>
 										</div>
 									</div>
